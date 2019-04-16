6e1c2c35dde591bd6c568c2d661e003dca511c3b
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-16 22:57:51 +0530

    feat: Add get all instructors route

--- startup/routers.js
+++ startup/routers.js
@@ -1,9 +1,11 @@
 const Express = require('express');
 const studentsRouter = require('../routes/students');
 const departmentsRouter = require('../routes/departments');
+const instructorsRouter = require('../routes/instructors');
 
 module.exports = (app) => {
     app.use(Express.json());
     app.use("/students", studentsRouter);
     app.use("/departments", departmentsRouter);
+    app.use("/instructors", instructorsRouter);
 }
