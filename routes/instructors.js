034263d764968034c335bf661a6512f9b050e517
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-17 01:46:14 +0530

    feat: Add ability to add new instructor

--- routes/instructors.js
+++ routes/instructors.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const tables = require('../db/tables');
 const db = require('../db/database').getDatabase();
+const { validateInstructor } = require('../db/models');
 
 router.get("/", (req, res) => {
     const sqlQuery = `SELECT * FROM ${tables.tableNames.instructor}`
@@ -78,5 +79,32 @@ router.get("/:id/department", (req, res) => {
         return res.send(row);
     })
 })
+
+router.post("/", (req, res) => {
+    const { error } = validateInstructor(req.body);
+    if (error) {
+        return res.status(400).send({
+            message: error.details[0].message
+        });
+    }
+
+    const instructorReq = req.body;
+    const sqlQuery = `
+    INSERT INTO ${tables.tableNames.instructor}
+    (name, salary, department_name)
+    VALUES ('${instructorReq.name}', ${instructorReq.salary}, '${instructorReq.department_name}')`;
+
+    db.run(sqlQuery, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({
+                message: "An error occured while trying to save the instructor details"
+            });
+        }
+        res.send({
+            message: "Instructor saved successfully."
+        });
+    });
+});
 
 module.exports = router;
