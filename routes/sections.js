0554ac765696e72d9885b2607e2f6f854faeb4f1
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-16 23:52:28 +0530

    feat: Add get section by id route

--- routes/sections.js
+++ routes/sections.js
@@ -14,4 +14,21 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+    const sqlQuery = `SELECT * FROM ${tables.tableNames.section} WHERE ${tables.sectionColumns.id} = ?`;
+    db.get(sqlQuery, [req.params.id], (err, rows) => {
+        if (err) {
+            return res.status(500).send({
+                message: "An error occurred."
+            });
+        }
+        if (!rows) {
+            return res.status(404).send({
+                message: "A section with the requested ID could not be found."
+            });
+        }
+        res.send(rows);
+    })
+})
+
 module.exports = router;
