1546a2683531a3abfc0626d33eafc4e8fea152ba
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-17 01:42:45 +0530

    feat: Add ability to create new sections

--- routes/sections.js
+++ routes/sections.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const tables = require('../db/tables');
 const db = require('../db/database').getDatabase();
+const { validateSection } = require('../db/models');
 
 router.get("/", (req, res) => {
     const sqlQuery = `SELECT * FROM ${tables.tableNames.section}`;
@@ -55,6 +56,33 @@ router.get("/:id/instructors", (req, res) => {
 
         res.send(rows);
     })
+});
+
+router.post("/", (req, res) => {
+    const { error } = validateSection(req.body);
+    if (error) {
+        return res.status(400).send({
+            message: error.details[0].message
+        });
+    }
+
+    const sectionReq = req.body;
+    const sqlQuery = `
+    INSERT INTO ${tables.tableNames.section}
+    (id, semester, year)
+    VALUES (${sectionReq.id}, ${sectionReq.semester}, ${sectionReq.year})`;
+
+    db.run(sqlQuery, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({
+                message: "An error occured while trying to save the section details"
+            });
+        }
+        res.send({
+            message: "Section saved successfully."
+        });
+    });
 });
 
 module.exports = router;
