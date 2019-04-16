e65cb28e8cae912ae5bda838ad492f6cc07bbb99
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-16 22:37:55 +0530

    feat: Add get student by id route

--- routes/students.js
+++ routes/students.js
@@ -8,10 +8,30 @@ router.get("/", (req, res) => {
     db.all(sqlQuery, (err, rows) => {
         if (err) {
             console.log(err);
-            return res.status(500).send("An error occurred");
+            return res.status(500).send({
+                message: "An error occurred"
+            });
         }
-        console.log(rows);
         res.send(rows);
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    const sqlQuery = `SELECT * FROM ${tables.tableNames.student} WHERE ${tables.studentColumns.id} = ${id}`;
+    db.get(sqlQuery, (err, rows) => {
+        if (err) {
+            console.log(err);
+            return res.status(404).send({
+                message: "An error occurred"
+            });
+        }
+        if (!rows) {
+            return res.status(404).send({
+                message: "A student with the requested ID was not found."
+            });
+        }
+        return res.send(rows);
     });
 });
 
