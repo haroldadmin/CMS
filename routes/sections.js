8d3158ddddefd1bd63abcda9dbeca220f132b824
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-16 23:27:40 +0530

    feat: Add get all sections route

---
+++ routes/sections.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const tables = require('../db/tables');
+const db = require('../db/database').getDatabase();
+
+router.get("/", (req, res) => {
+    const sqlQuery = `SELECT * FROM ${tables.tableNames.section}`;
+    db.all(sqlQuery, (err, rows) => {
+        if (err) {
+            return res.status(500).send({
+                message: "An error occurred."
+            })
+        }
+        res.send(rows);
+    });
+});
+
+module.exports = router;
--- startup/routers.js
+++ startup/routers.js
@@ -2,10 +2,12 @@ const Express = require('express');
 const studentsRouter = require('../routes/students');
 const departmentsRouter = require('../routes/departments');
 const instructorsRouter = require('../routes/instructors');
+const sectionsRouter = require('../routes/sections');
 
 module.exports = (app) => {
     app.use(Express.json());
     app.use("/students", studentsRouter);
     app.use("/departments", departmentsRouter);
     app.use("/instructors", instructorsRouter);
+    app.use("/sections", sectionsRouter);
 }
